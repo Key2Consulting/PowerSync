166d13ae94fe564c743668d6f74ef79660beddd0
Matt Wollner <wollmatt@hotmail.com>
2018-08-01 13:43:37 -0400

    Added CSV Logging

--- Script/Provider/Log/TextLogProvider.ps1
+++ Script/Provider/Log/TextLogProvider.ps1
@@ -1,9 +1,27 @@
 class TextLogProvider : LogProvider {
 
+    [string] $FilePath 
+
     TextLogProvider ([string] $Namespace, [hashtable] $Configuration) : base($Namespace, $Configuration) {
+        $this.FilePath = $this.ConnectionStringParts['filepath']   
+       # $this.InitLog()
     }
 
-    [void] WriteLog([datetime] $MessageDate, [string] $MessageType, [string] $Message, [string] $VariableName, [object] $VariableValue) {
+    [void] WriteLog([string] $LogID, [string] $ParentLogID, [datetime] $MessageDate, [string] $MessageType, [string] $Message, [string] $VariableName, [object] $VariableValue) {
+        
+        # Create a hash for the log data
+        $LogRow = [pscustomobject]@{
+            LogID = $LogID
+            ParentLogID = $ParentLogID
+            MessageDate = $MessageDate
+            MessageType = $MessageType
+            Message = $Message
+            VariableName = $VariableName
+            VariableValue = $VariableValue
+        }
+
+        $LogRow  | Export-Csv -Path $this.FilePath -NoTypeInformation -Append
+
     }
     
     [void] ArchiveLog([int] $ExpirationInDays) {
